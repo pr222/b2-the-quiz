47970e5a761660be7bc61b0521c24b9956e65cea
Pauliina Raitaniemi <pr222ja@student.lnu.se>
2020-11-23 16:45:35 +0100

    Add base structure for components

--- src/components/countdown-timer/countdown-timer.js
+++ src/components/countdown-timer/countdown-timer.js
@@ -4,3 +4,75 @@
  * @author Pauliina Raitaniemi <pr222ja@student.lnu.se>
  * @version 1.0.0
  */
+
+/**
+ * Define the template.
+ */
+const template = document.createElement('template')
+template.innterHTML = ` 
+<style>
+
+</style>
+
+<div>
+
+</div>
+`
+/**
+ * Define the custom element.
+ */
+customElements.define('',
+/**
+ * Anonymous class for the element.
+ */
+  class extends HTMLElement {
+    /**
+     * Make an instance of this type.
+     */
+    constructor () {
+      super()
+
+      // Attach shadow root and append template.
+      this.attachShadow({ mode: 'open' })
+        .appendChild(template.content.cloneNode(true))
+
+      //
+    }
+
+    /**
+     * Looks out for changes in attributes.
+     *
+     * @returns {string[]} - An array with stings of the attibutes.
+     */
+    static get observedAttributes () {
+      return ['']
+    }
+
+    /**
+     * Called when the element has been insterted into the DOM.
+     */
+    connectedCallback () {
+      //
+    }
+
+    /**
+     * Called when the element has been removed from the DOM.
+     */
+    disconnectedCallback () {
+      //
+    }
+
+    /**
+     * Called by the browser when an attribute is changed.
+     *
+     * @param {string} name - The name of the attribute.
+     * @param {any} oldValue - The old attribute value.
+     * @param {any} newValue - The new attribute.
+     */
+    attributeChangedCallback (name, oldValue, newValue) {
+      //
+    }
+
+    //
+  }
+)
